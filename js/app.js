ae43d9d9d5802c288370215557c247af4a9757f7
Rachel Conner <rconner614@yahoo.com>
2016-01-29 17:18:01 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -4,12 +4,37 @@
   ]);
 
   app.controller('myController', myController);
-  myController.$inject = ['$scope'];
-  function myController($scope){
+  myController.$inject = ['$scope', '$timeout'];
+  function myController($scope, $timeout){
       $scope.copyright = new Date();
       $scope.isHeating = true;
       $scope.kill = false;
-      $scope.current = 130.2;
+      $scope.current = 80;
+      $scope.settings = {
+          targetTemp: 160.00,
+          threshold: 4.00,
+          killThreshold: 20.00,
+          updateKey: null
+      };
+      $scope.findTemp = function(){
+          var hue = (($scope.current / 220) * 175) + 200;
+          $scope.tempColor = 'hsl(' + hue + ', 60%, 50%)';
+      };
+
+      $scope.temp = function(){
+          if($scope.isHeating){
+              $scope.current = (Number($scope.current) + (Math.random() * 2 - 1)).toFixed(2);
+          } else{
+              $scope.current = (Number($scope.current) - (Math.random() *.5)).toFixed(2);
+          }
+
+          $timeout(function(){
+              $scope.temp();
+              $scope.findTemp();
+          }, 2000);
+      };
+      $scope.temp();
+      $scope.findTemp();
   }
 
 })();
