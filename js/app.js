53b5875aa623df7e914417ee7b3a6dae03a39671
Rachel Conner <rconner614@yahoo.com>
2016-01-29 18:04:47 -0500

    gradient temp

--- js/app.js
+++ js/app.js
@@ -9,13 +9,15 @@
       $scope.copyright = new Date();
       $scope.isHeating = true;
       $scope.kill = false;
-      $scope.current = 80;
+      $scope.current = 160;
+      $scope.needle = ($scope.current / 220) * 360;
       $scope.settings = {
           targetTemp: 160.00,
           threshold: 4.00,
           killThreshold: 20.00,
           updateKey: null
       };
+      console.log($scope.needle);
       $scope.findTemp = function(){
           var hue = (($scope.current / 220) * 175) + 200;
           $scope.tempColor = 'hsl(' + hue + ', 60%, 50%)';
