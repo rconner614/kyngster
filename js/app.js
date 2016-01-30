45b3fdb70e304c7b099cc3e1b226e42a6cf6aac2
Rachel Conner <Rachel Conner>
2016-01-30 10:56:29 -0500

    updates to fix sizing issues for columns and svg. no longer the same height but that will look better when there are more than two tiles.

--- js/app.js
+++ js/app.js
@@ -10,14 +10,26 @@
       $scope.isHeating = true;
       $scope.kill = false;
       $scope.current = 160;
-      $scope.needle = ($scope.current / 220) * 360;
       $scope.settings = {
           targetTemp: 160.00,
           threshold: 4.00,
           killThreshold: 20.00,
           updateKey: null
       };
-      console.log($scope.needle);
+      $scope.needle = function () {
+          return 360 * (1-($scope.current / 212));
+      };
+
+      $scope.adjustHeight = function(){
+          var bBox = svg1.getBBox();
+          console.log('XxY', bBox.x + 'x' + bBox.y);
+          console.log('size', bBox.width + 'x' + bBox.height);
+      }
+
+      $scope.targetNeedle = function () {
+          return 360 * (1-($scope.settings.targetTemp / 212));
+      };
+
       $scope.findTemp = function(){
           var hue = (($scope.current / 220) * 175) + 200;
           $scope.tempColor = 'hsl(' + hue + ', 60%, 50%)';
